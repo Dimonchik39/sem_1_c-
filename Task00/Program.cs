5836d4e547a2ddb028bb62c64708f5a3fc3c1dc6
Dimonchik <sudjadred@live.com>
2022-08-31 23:25:55 +0200

    написали задачку - число в квадрате

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите пожалуйста целое число для получения его квадрата");
+int num = Convert.ToInt32(Console.ReadLine());
+int sq = num * num;
+Console.WriteLine($"Готово! {num} = {sq}");
+

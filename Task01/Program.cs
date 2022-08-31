80f3ac5c5c8196c7c3f5d5d3e6271f3e94ee4897
Dimonchik <sudjadred@live.com>
2022-09-01 01:04:44 +0200

    вторая задачка семинара

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("ВВедите 2 числа. Мы определим, является ли первое число квадратом второго.");
+Console.Write("Введите первое число:  ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число:  ");
+int numB = Convert.ToInt32(Console.ReadLine());
+int sq = numB * numB;
+if (sq == numA) Console.WriteLine("Да, это оно!");
+if (sq != numA) Console.WriteLine("Оу, это не оно!");

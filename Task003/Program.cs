f1cacf76000a03fe3a81224a9675ff500682d5ab
Dimonchik <sudjadred@live.com>
2022-09-01 23:06:24 +0200

    задачка про дни недели

---
+++ Task003/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите число и узанаете день недели: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num == 1) Console.WriteLine("Среда");
+else if (num == 2) Console.WriteLine("Среда");
+else if (num == 3) Console.WriteLine("Среда");
+else if (num == 4) Console.WriteLine("Четверг");
+else if (num == 5) Console.WriteLine("Пятница");
+else if (num == 6) Console.WriteLine("Суббота");
+else if (num == 7) Console.WriteLine("Воскресенье");
+else Console.WriteLine("Еще не придумали такого дня");

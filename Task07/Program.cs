4b6c020373c1502a3be6d97af919b9872a3d1299
Dimonchik <sudjadred@live.com>
2022-09-02 00:05:12 +0200

    Задачка про трехзначное число

---
+++ Task07/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите трехзначное число, чтобы узнать последнюю цифру: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num < 1000 && num > 99)
+{
+    num = num %10;
+    Console.WriteLine(num);
+} 
+else
+{
+    Console.WriteLine("Это не трехзначное число");
+}

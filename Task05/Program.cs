018161eeb6724b587215eb00722303d114d630db
Dimonchik <sudjadred@live.com>
2022-09-02 22:41:17 +0200

    Task05_added

---
+++ Task05/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите число, чтобы увидеть все целые промежуточные числа: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = -num;
+while (count <= num)
+{
+    Console.Write($"{count}, ");
+    count ++;
+}
